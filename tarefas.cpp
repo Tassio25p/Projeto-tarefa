649360baf5e20e016f91bf18245537ffedaa35e8
Tassio25p <159667701+Tassio25p@users.noreply.github.com>
2024-02-12 14:41:30 -0300

    Update tarefas.cpp

--- tarefas.cpp
+++ tarefas.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <locale>
-//#include <string>
+#include <string>
 using namespace std;
 
 main(){
@@ -8,12 +8,12 @@ setlocale (LC_ALL,"Portuguese");
 string nome;
 float Pimpostos,Prevededor,Pfabrica,Pfinal;
  cout<<"Digite o nome do carro :";cin>>nome;
- cout<<"Digite o Preço de Fábrica:";cin>>Pfabrica;
+ cout<<"Digite o PreÃ§o de FÃ¡brica:";cin>>Pfabrica;
         Pimpostos=(Pfabrica)*0.45;
         Prevededor=(Pfabrica)*0.28;
         Pfinal=(Pfabrica)+(Pimpostos)+(Prevededor);
  
-   cout<<"O nome do automovel è:"<<nome<<endl;
+   cout<<"O nome do automovel Ã¨:"<<nome<<endl;
    cout<<"O automovel custa:"<<Pfinal<<endl;
    system("pause");
 }
